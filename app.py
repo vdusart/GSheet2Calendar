34db206ecac95a75a7c1f9cae201b1006ec99679
Lucas DRAESCHER <lucas.draescher@gmail.com>
2022-09-24 11:16:36 +0200

    fixup! fixup! Testing CRON

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     return render_template('index.html', link="test")
 
 
-@app.route('/', defaults={ 'path': '' })
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return redirect('/')
