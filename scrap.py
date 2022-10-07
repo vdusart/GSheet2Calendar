1eb0ec2cf10ce5d72183f467f301984c311c1e1d
Lucas DRAESCHER <lucas.draescher@gmail.com>
2022-10-07 10:57:56 +0200

    Merged scrap and upload

--- scrap.py
+++ scrap.py
@@ -1,11 +1,19 @@
-import shutil
 import os
+import shutil
 import requests
+from os.path import isfile, join
 from ics import Calendar
+from boto.s3.key import Key
+from boto.s3.connection import S3Connection
+from boto.s3.connection import OrdinaryCallingFormat
 
 from Parser import Parser
 from models.db import Timetables
 from utils.base import Base, Session, engine
+
+api_key = os.environ.get('CELLAR_ADDON_KEY_ID')
+secret_key = os.environ.get('CELLAR_ADDON_KEY_SECRET')
+host = os.environ.get('CELLAR_ADDON_HOST')
 
 Base.metadata.create_all(engine)
 session = Session()
@@ -30,3 +38,18 @@ for timetable in timetables:
         lines = c.serialize_iter()
         lines = lines[:-2] + ["REFRESH-INTERVAL;VALUE=DURATION:P1H\n"] + lines[-1:]
         f.writelines(lines)
+
+cf = OrdinaryCallingFormat()  # Can't use uppercase name
+conn = S3Connection(aws_access_key_id=api_key, aws_secret_access_key=secret_key, host=host, calling_format=cf)
+
+bucket = conn.get_bucket(os.environ.get('BUCKET_NAME'))
+bucket.set_acl('public-read')
+
+directory = os.environ.get('ICS_FOLDER_NAME')
+calendars = [f for f in os.listdir(directory) if isfile(join(directory, f))]
+
+for calendar in calendars:
+    key = Key(bucket)
+    key.key = calendar
+    key.set_contents_from_filename(f'{directory}/{calendar}')
+    key.set_acl('public-read')
