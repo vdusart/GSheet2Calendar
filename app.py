41e6b7b4b88e7fb0bf80ff35ab8771c1bb51c91b
dusartvict <dusartvict@eisti.eu>
2022-10-17 11:58:25 +0200

    Add style

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
+url = "https://cytech-calendars.cellar-c2.services.clever-cloud.com/{0}"
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', link="test")
+    return render_template('index.html', linkA=url.format("ING3-CS-A.ics"), linkB=url.format("ING3-CS-B.ics"))
 
 
 @app.route('/', defaults={'path': ''})
